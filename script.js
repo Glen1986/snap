5b26c1f42d532e5eaad742275e01860ccdf8f924
glen1986 <escobar.glen@gmail.com>
2021-06-21 13:35:03 -0300

    primera modificacion

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ let str1 = "Snap";
 let str2 = "Crackle";
 let str3 = "Prime";
 
+function snapCrackle(maxValue){
+  let output = '';
+  //loop de 1 hasta el max...
+        for(let i = 0; i <= maxValue; i++){
+  //impar = "snap";
+          if( i % 2 === 1 ){
+            output += "Snap"
+          }
+  //multiplo de 5 = "crackle";
+          if(i % 5 === 0){
+            output += "Crackle";
+          }
+  //multiplo de 3 y de 5 = "snapCracke";
+         // if( i % 2 === 1 && i % 5 === 0 ){
+        //    output += "SnapCrackle"
+        //  }
+          if(!(i % 2 === 1 || i % 5 === 0)){
+            output += i;
+          }
+          output +=", "
+        }
+  //si no es uno u otro, colocar el proio numero;
+  //todo separad por ", "
+        return output
+      }
+
+/*
 function snapCrackle(maxValue) {
   let str4 = [];
   for (let i = 1; i <= maxValue; i++) {
@@ -17,9 +44,13 @@ function snapCrackle(maxValue) {
   }
   return str4.toString().replace(", ", " ").replace(" ","");
 }
+let output = snapCrackle(12);
+console.log(output);
 
-snapCrackle(12);
-//console.log(snapCrackle());
+
+
+
+
 
 function snapCracklePrime(maxValue) {
   let str4 = [];
@@ -39,4 +70,4 @@ function snapCracklePrime(maxValue) {
   return str4.toString().replace(", ", " ").replace(" ","");
  // console.log(str4);
 }
-snapCracklePrime();
+snapCracklePrime();*/
